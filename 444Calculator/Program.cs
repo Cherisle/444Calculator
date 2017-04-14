429727d7113e2716acca9d61ed22b323d3f9053c
Cherisle <michaeltran95@live.com>
2017-04-14 10:45:41 -0700

    Completed MenuLoop for Calculator

--- 444Calculator/Program.cs
+++ 444Calculator/Program.cs
@@ -10,7 +10,56 @@ namespace _444Calculator
     {
         static void Main(string[] args)
         {
-            Console.ReadKey();
+            bool menuDone = false;
+            string menuChoice = "";
+            Console.Write("This is the 444 Simple Complex Number Calculator.");
+            Console.WriteLine("What would you like to do?");
+            while(!menuDone)
+            {
+                Console.WriteLine("1. Add\n2. Div\n3. Mag\n4. Ang");
+                Console.WriteLine("Enter anything else to quit.");
+                ConsoleKeyInfo key = Console.ReadKey();
+                Console.WriteLine();
+                menuChoice = key.KeyChar.ToString();
+                switch(menuChoice)
+                {
+                    case "1":
+                        addFunction();
+                        break;
+                    case "2":
+                        divFunction();
+                        break;
+                    case "3":
+                        magFunction();
+                        break;
+                    case "4":
+                        angFunction();
+                        break;
+                    default:
+                        menuDone = true;
+                        break;              
+                }
+            }
+        }
+
+        static void addFunction()
+        {
+
+        }
+
+        static void divFunction()
+        {
+
+        }
+
+        static void magFunction()
+        {
+
+        }
+
+        static void angFunction()
+        {
+
         }
     }
 }
