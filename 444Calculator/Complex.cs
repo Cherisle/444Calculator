4bcf30e68f24d30c8ad1a4443148d2b8de7eff1a
Cherisle <michaeltran95@live.com>
2017-05-19 00:42:03 -0700

    massive changes incoming
    yakno da dealio

---
+++ 444Calculator/Complex.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _444Calculator
+{
+    class Complex
+    {
+        public void Complex_Calculator()
+        {
+            bool complexMenuDone = false;
+            string menuChoice = "";
+            string x, y, u, v, s;
+            x = y = u = v = s = "";
+            Console.WriteLine("This is the 444 Simple Complex Number Calculator.");
+            Console.WriteLine("What would you like to do?(Options are case-sensitive)\n");
+            while (!complexMenuDone)
+            {
+                Console.WriteLine("add\ndiv\nmag\nang\n");
+                Console.WriteLine("Enter anything else to quit.");
+                menuChoice = Console.ReadLine();
+
+                switch (menuChoice)
+                {
+                    case "add":
+                        addFunction(x, y, u, v, s);
+                        break;
+                    case "div":
+                        divFunction(x, y, u, v, s);
+                        break;
+                    case "mag":
+                        magFunction(x, y, u, v, s);
+                        break;
+                    case "ang":
+                        angFunction(x, y, u, v, s);
+                        break;
+                    default:
+                        complexMenuDone = true;
+                        break;
+                }
+            }
+        }
+
+        static void addFunction(string x, string y, string u, string v, string s)
+        {
+            standardDisplay();
+            Console.Write("What is x? ");
+            x = Console.ReadLine();
+            if (x.Contains("^")) { x = "(" + x + ")"; }
+
+            Console.Write("What is y? ");
+            y = Console.ReadLine();
+            if (y.Contains("^")) { y = "(" + y + ")"; }
+
+            Console.Write("What is u? ");
+            u = Console.ReadLine();
+            if (u.Contains("^")) { u = "(" + u + ")"; }
+
+            Console.Write("What is v? ");
+            v = Console.ReadLine();
+            if (v.Contains("^")) { v = "(" + v + ")"; }
+
+            // start to simplify here
+
+            x = simplify(x); y = simplify(y); u = simplify(u); v = simplify(v);
+            s = "[" + x + "," + y + "i]+[" + u + "," + v + "i]";
+            Console.WriteLine(s);
+
+            Console.WriteLine("Evaluated Result: {0}", evaluate(s));
+        }
+
+        static void divFunction(string x, string y, string u, string v, string s)
+        {
+            standardDisplay();
+            Console.Write("What is x? ");
+            x = Console.ReadLine();
+            Console.Write("What is y? ");
+            y = Console.ReadLine();
+            Console.Write("What is u? ");
+            u = Console.ReadLine();
+            Console.Write("What is v? ");
+            v = Console.ReadLine();
+
+            // start to simplify here
+
+            x = simplify(x); y = simplify(y); u = simplify(u); v = simplify(v);
+            s = "[" + x + "," + y + "i]/[" + u + "," + v + "i]";
+            Console.WriteLine(s);
+
+            Console.WriteLine("Evaluated Result: {0}", evaluate(s));
+        }
+
+        static void magFunction(string x, string y, string u, string v, string s)
+        {
+            displayForOneEquation();
+            Console.Write("What is x? ");
+            x = Console.ReadLine();
+            Console.Write("What is y? ");
+            y = Console.ReadLine();
+
+            x = simplify(x); y = simplify(y);
+            s = "(|" + x + "+" + y + "i|)";
+            Console.WriteLine(s);
+
+            Console.WriteLine("Evaluated Result: {0}", evaluate(s));
+        }
+
+        static void angFunction(string x, string y, string u, string v, string s)
+        {
+            displayForOneEquation();
+            Console.Write("What is x? ");
+            x = Console.ReadLine();
+            Console.Write("What is y? ");
+            y = Console.ReadLine();
+            x = simplify(x); y = simplify(y);
+            s = "ang(" + y + "/" + x + "i)";
+            Console.WriteLine(s);
+            Console.WriteLine("Evaluated Result in radians: {0}", evaluate(s));
+
+        }
+
+        static void standardDisplay()
+        {
+            Console.WriteLine("First Complex Number: x+yi");
+            Console.WriteLine("Second Complex Number: u+vi");
+        }
+
+        static void displayForOneEquation()
+        {
+            Console.WriteLine("First Complex Number: x+yi");
+        }
+
+        static string simplify(string s)
+        {
+            if (s.Contains("^"))
+            {
+                char[] powerDelims = { ' ', '(', ')', '^' };
+                string[] powerContents = s.Split(powerDelims);
+
+                double baseNum = Int32.Parse(powerContents[1]);
+                double powerNum = Int32.Parse(powerContents[2]);
+
+                s = Math.Pow(baseNum, powerNum).ToString();
+            }
+            return s;
+        }
+
+        static string evaluate(string s)
+        {
+            char[] equationDelims = { '[', ']', '(', ')', ',', 'i' };
+            string[] equationContents = s.Split(equationDelims);
+            if (equationContents.Contains("+"))
+            {
+                string realNums = (Int32.Parse(equationContents[1]) + Int32.Parse(equationContents[5])).ToString();
+                string imaginaryNums = (Int32.Parse(equationContents[2]) + Int32.Parse(equationContents[6])).ToString();
+
+                s = "" + realNums + " + " + imaginaryNums + "i";
+            }
+
+            if (equationContents.Contains("/") && !equationContents.Contains("ang"))
+            {
+                double numerator1, numerator2, denominator = 0;
+                string fraction1 = "";
+                string fraction2 = "";
+                numerator1 = ((Int32.Parse(equationContents[1]) * Int32.Parse(equationContents[5]))
+                                   + (Int32.Parse(equationContents[2]) * Int32.Parse(equationContents[6])));
+                numerator2 = ((Int32.Parse(equationContents[2]) * Int32.Parse(equationContents[5]))
+                                   - (Int32.Parse(equationContents[1]) * Int32.Parse(equationContents[6])));
+                denominator = ((Int32.Parse(equationContents[5]) * Int32.Parse(equationContents[5]))
+                                  + (Int32.Parse(equationContents[6]) * Int32.Parse(equationContents[6])));
+                if (denominator == 0)
+                {
+                    Console.WriteLine("Calculation cannot be done because you are dividing by 0");
+                }
+                else
+                {
+                    fraction1 = (numerator1 / denominator).ToString();
+                    fraction2 = (numerator2 / denominator).ToString();
+                    if (fraction2.Contains("-")) { s = "" + fraction1 + fraction2 + "i"; }
+                    else { s = "" + fraction1 + " + " + fraction2 + "i"; }
+                }
+
+            }
+
+            if (equationContents.Contains("|"))
+            {
+                char[] secondParse = { '+', '-', '|', ')', '(' };
+                equationContents = s.Split(secondParse);
+                equationContents[3] = equationContents[3].Trim('i');
+                Console.WriteLine(equationContents[2] + "\n" + equationContents[3]);
+                double magVal = Math.Pow((Math.Pow(Int32.Parse(equationContents[2]), 2) +
+                                Math.Pow(Int32.Parse(equationContents[3]), 2)), 0.5);
+                s = "" + magVal;
+            }
+
+            if (equationContents.Contains("ang"))
+            {
+                char[] angParse = { 'a', 'n', 'g', '(', '/', ')', 'i' };
+                equationContents = s.Split(angParse);
+
+                double radians = Math.Atan2(Int32.Parse(equationContents[4]), Int32.Parse(equationContents[5]));
+                double angle = radians * (180 / Math.PI);
+                Console.WriteLine("Evaluated Result in degrees: {0}", angle);
+                s = "" + radians;
+            }
+            return s;
+        }
+    }
+}
---
+++ 444Calculator/Simple.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _444Calculator
+{
+    class Simple
+    {
+        public void Simple_Calculator()
+        {
+            string equation = "";
+            char[] delimiters = { ' ' };
+            string[] calculations;
+
+            Console.WriteLine("Enter in a mathematical equation to be computed.");
+            Console.WriteLine("For the options, you can enter 'help'.");
+            equation = Console.ReadLine();
+
+            if (equation.Contains("+"))
+            {
+                char[] additionDelim = { '+' };
+                calculations = equation.Split(additionDelim);
+                int sum = 0;
+                for (int i = 0; i < calculations.Length; i++)
+                {
+                    sum += Int32.Parse(calculations[i]);
+
+                }
+                Console.WriteLine("Sum of {0}: {1}", equation, sum);
+            }
+
+            if (equation.Contains("-") && !equation.Contains("^"))
+            {
+                char[] subtractionDelim = { '-' };
+                calculations = equation.Split(subtractionDelim);
+                int difference = 0;
+                for (int i = 0; i < calculations.Length; i++)
+                {
+                    difference -= Int32.Parse(calculations[i]);
+
+                }
+                Console.WriteLine("Difference of {0}: {1}", equation, difference);
+            }
+
+            if (equation.Contains("equal"))
+            {
+                calculations = equation.Split(delimiters);
+                if (equation.Contains("not"))
+                {
+                    if (Int32.Parse(calculations[0]) != Int32.Parse(calculations[5]))
+                        Console.WriteLine("{0}?: true", equation);
+                    else
+                        Console.WriteLine("{0}?: false", equation);
+                }
+                else if(equation.Contains("^"))
+                {
+                    char[] delims = {' '};
+                    calculations = equation.Split(delims);
+                    string pemdasSolveP = ""; // pemdas solve parenthesis
+                    string outsideParenthesis = "";
+                    foreach (var el in calculations)
+                    {
+                        if(el.Contains("(") || el.Contains(")"))
+                        {
+                            char[] innerDelim = { ')' };
+                            string[] calculations2 = el.Split(innerDelim);
+                            outsideParenthesis = calculations2[1];
+                            foreach (var item in calculations2)
+                            {
+                                if(item.Contains("("))
+                                {
+                                    string trimmed = item.Trim('(');
+                                    pemdasSolveP = pemdasSolveP + trimmed;
+                                }
+                            }                        
+                        }
+                    }
+                    char[] delims2 = {'^'}; // delim by ^ to isolate exponented value
+                    string [] calculations3 = pemdasSolveP.Split(delims2);
+                    char[] delims3 = { '*', '/', '+', '-' };
+                    string[] calculations4 = calculations3[0].Split(delims3);
+                    string pemdasSolveE = calculations4[1] +  "^"; //solving for E in pemdas
+                    double solved = 0;
+                    if(calculations3[1].Contains("/")) // detected fraction, simplify before putting into method
+                    {
+                        char[] divisionDelim = { '/' };
+                        string[] calculations5 = calculations3[1].Split(divisionDelim);
+                        double decimalResult = (Double.Parse(calculations5[0]) / Double.Parse(calculations5[1]));
+                        pemdasSolveE = pemdasSolveE + decimalResult;
+                    }                  
+                    pemdasSolveE = simplify(pemdasSolveE); // solved E, case back into pemdasP
+                    if (pemdasSolveP.Contains("*"))
+                    {
+                        solved = Double.Parse(calculations4[0]) * Double.Parse(pemdasSolveE);
+                    }
+                    else if (pemdasSolveP.Contains("/"))
+                    {
+                        solved = Double.Parse(calculations4[0]) / Double.Parse(pemdasSolveE);
+                    }
+                    else if (pemdasSolveP.Contains("+"))
+                    {
+                        solved = Double.Parse(calculations4[0]) + Double.Parse(pemdasSolveE);
+                    }
+                    else if (pemdasSolveP.Contains("-"))
+                    {
+                        solved = Double.Parse(calculations4[0]) - Double.Parse(pemdasSolveE);
+                    }
+                    pemdasSolveP = solved.ToString();
+                    string pemdasSolvePE = pemdasSolveP + outsideParenthesis;
+                    pemdasSolvePE = simplify(pemdasSolvePE); // final answer
+                    if (Double.Parse(pemdasSolvePE) == Double.Parse(calculations[4]))
+                        Console.WriteLine("Is {0}?: true", equation);
+                    else
+                        Console.WriteLine("Is {0}?: false", equation);
+                }
+                else
+                {
+                    if (Int32.Parse(calculations[0]) == Int32.Parse(calculations[4]))
+                        Console.WriteLine("Is {0}?: true", equation);
+                    else
+                        Console.WriteLine("Is {0}?: false", equation);
+                }
+            }
+
+            if (equation.Contains("/") && !equation.Contains("equal") && !equation.Contains("^"))
+            {
+                char[] divisionDelim = { '/' };
+                calculations = equation.Split(divisionDelim);
+                double remainder = (Double.Parse(calculations[0]) % Double.Parse(calculations[1]));
+                int quotient = (int)(Double.Parse(calculations[0]) / Double.Parse(calculations[1]));
+                Console.WriteLine("Remainder of {0}: {1}", equation, remainder);
+                Console.WriteLine("Whole Number quotient of {0}: {1}", equation, quotient);
+            }
+
+            if(equation.Contains("and") && !equation.Contains("r"))
+            {
+                calculations = equation.Split(new[] { " and " }, StringSplitOptions.None);
+                double token1 = Double.Parse(simplify(calculations[0]));
+                double token2 = Double.Parse(simplify(calculations[1]));
+                Console.WriteLine("Greater value between {0} and {1}: {2}",token1,token2,Math.Max(token1, token2));
+            }
+
+            if(equation.Contains("random value between"))
+            {
+                char[] commaDelim = { ',' };
+                calculations = equation.Split(new[] { "random value between " }, StringSplitOptions.None);
+                string[] bounds = calculations[1].Split(commaDelim);
+                Random rand = new Random();
+                Console.WriteLine("Random value between {0} and {1}: {2}", 
+                    bounds[0], bounds[1], rand.Next(Int32.Parse(bounds[0]),Int32.Parse(bounds[1])));
+            }
+
+            if(equation.Contains("^") && !equation.Contains("equal"))
+            {
+                Console.WriteLine("Value of {0}, {1}",equation,simplify(equation));
+            }
+        }
+
+        string simplify(string s)
+        {
+            if (s.Contains("^"))
+            {
+                char[] powerDelims = {'^'};
+                string[] powerContents = s.Split(powerDelims);
+
+                double baseNum = Double.Parse(powerContents[0]);
+                double powerNum = Double.Parse(powerContents[1]);
+
+                s = Math.Pow(baseNum, powerNum).ToString();
+            }
+            return s;
+        }
+    }
+}
